e385b91054e8594603aae6673d4d043ff5b203de
puckbee <puckbee@gmail.com>
2018-08-28 22:01:52 +0800

    add some loops for demonstation

--- gemm.cpp
+++ gemm.cpp
@@ -18,19 +18,23 @@ int main(int argc, char** argv)
 
     for(int i=0; i< D*D; i++)
     {
-        A[i] = (double(i%100)/2) * (double(i%100)/2);
-        B[i] = (double(i%100)/4) * (double(i%100)/4);
-        C[i] = (double(i%100)/2);
+//        A[i] = (double(i%100)/2) * (double(i%100)/2);
+//        B[i] = (double(i%100)/4) * (double(i%100)/4);
+//        C[i] = (double(i%100)/2);
+        A[i] = i%2;
+        B[i] = i%3;
+        C[i] = 0;
     }
 
 
-//  loop1 with automatic parallel pragma 
+//  loop1 with automatic parallel pragma (collapse may cause write conflict, but normally fine.)
     #pragma omp parallel for
     for (int i=0; i<D; i++)
         for(int j=0; j<D; j++)
             for(int k=0; k<D; k++)
                 C[i*D+j] += A[i*D+k] * B[k*D+j];
 // end of loop1
+
 
 /*
 // loop2 with mannually blocking
@@ -50,6 +54,49 @@ int main(int argc, char** argv)
 //end of loop2
 */
 
+/*    
+//  loop3 with write confliction (the logic is wrong, just for demonstrating)
+    #pragma omp parallel for
+    for (int i=0; i<D; i++)
+        for(int j=0; j<D; j++)
+            for(int k=0; k<D; k++)
+                C[i%2] += A[i*D+k] * B[k*D+j];
+// end of loop3
+*/
+ 
+/*    
+// loop4 without reduction (there are write conflict on 'sum', so the result would be wrong)
+    double sum = 0;
+    #pragma omp parallel for
+    for(int i=0; i<D; i++)
+        sum += i%10;
+    printf(" sum = %f \n", sum);
+ //end of loop4
+*/
+
+/*
+// loop5 with reduction 
+    double sum = 0;
+    #pragma omp parallel for reduction(+:sum)
+    for(int i=0; i<D; i++)
+        sum += i%10;
+    printf(" sum = %f \n", sum);
+ //end of loop5
+*/
+
     printf(" C[0] = %f\n", C[0]);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
